006054a80eb120f4ca89af5f42fcf854bc8850be
David Olejniczak <david.olejniczak@icloud.com>
2026-02-26 19:30:21 -0500

    Initial Commit

---
+++ SimpleMath/SimpleMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMathApp.swift
+//  SimpleMath
+//
+//  Created by David Olejniczak on 2026-02-26.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleMathTests/SimpleMathTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMathTests.swift
+//  SimpleMathTests
+//
+//  Created by David Olejniczak on 2026-02-26.
+//
+
+import Testing
+@testable import SimpleMath
+
+struct SimpleMathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
